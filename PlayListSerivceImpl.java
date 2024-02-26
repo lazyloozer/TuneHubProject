ae3be43f653acbf21a93401816089868b717cc34
Manohar c <114498086+lazyloozer@users.noreply.github.com>
2024-02-26 19:29:34 +0530

    Create PlayListSerivceImpl.java
    1.adding songs to playlist and saving in the database.
    2.get playlist songs by checking premium

---
+++ PlayListSerivceImpl.java
@@ -0,0 +1,30 @@
+package com.kodnest.tunehub.serviceimpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kodnest.tunehub.entity.Playlist;
+import com.kodnest.tunehub.repository.PlayListRepositiory;
+import com.kodnest.tunehub.service.PlayListService;
+
+@Service
+public class PlayListServiceImpl implements PlayListService  {
+	@Autowired
+	PlayListRepositiory PlayListRepositiory;
+	
+	//adding songs to playlist and saving in the database
+	public void addplaylist(Playlist playlist) {
+		PlayListRepositiory.save(playlist);
+		
+	}
+
+	//get playlist songs by checking Premium
+	public List<Playlist> getplaylist() {
+		// TODO Auto-generated method stub
+		return PlayListRepositiory.findAll();
+	}
+	
+
+}
