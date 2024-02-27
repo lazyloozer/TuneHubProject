ed81bc225a516d9982e8be3db8fb16aacb04f30f
Manohar c <114498086+lazyloozer@users.noreply.github.com>
2024-02-27 18:35:07 +0530

    Create PlaylistSeriveinterface.java
    checking duplicate values

---
+++ PlaylistSeriveinterface.java
@@ -0,0 +1,18 @@
+package com.kodnest.tunehub.service;
+
+import java.util.List;
+
+import com.kodnest.tunehub.entity.Playlist;
+
+public interface PlayListService {
+
+	//public List<Playlist> getsongs();
+
+	public void addplaylist(Playlist playlist);
+
+	public List<Playlist> getplaylist();
+
+	public boolean songExits(String name);
+	
+
+}
